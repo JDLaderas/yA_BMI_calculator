ae483c21ad974fb47e3025074149c28283a3d862
JDLaderas <johnladeras21@gmail.com>
2023-03-31 05:21:17 +0400

    added some if else statements

--- BMI.py
+++ BMI.py
@@ -1,8 +1,21 @@
 print("This is a BMI Calculator\n")
 
-weight = float(input("Please enter the weight "))
-height = float(input("Please enter the height "))
+height = float(input("Please enter the height in m: "))
+weight = float(input("Please enter the weight in kg: "))
 
-calc = int(weight / (height * height))
+calc = (weight / (height * height))
 
-print("your BMI is: ", calc)
+result = int("{:.0f}".format(calc))
+
+print("your BMI is:",result)
+
+if result <= 18.4:
+    print(f"Your BMI is {result}, you are underweight.")
+elif result <= 25:
+    print(f"Your BMI is {result}, you have a normal weight.")
+elif result <= 30:
+    print(f"Your BMI is {result}, you are slightly overweight.")
+elif result <= 35:
+    print(f"Your BMI is {result}, you are obese.")
+else:
+    print(f"Your BMI is {result}, you are clinically obese.")
